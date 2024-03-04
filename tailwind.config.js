a59cf74175db94ac971329c4ed40c048818e4b56
Victor Akira <akira04@gmail.com>
2024-03-04 01:48:09 -0300

    change color to purple

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple-heart": {
+          50: "#eef0ff",
+          100: "#e1e4fe",
+          200: "#c8cdfd",
+          300: "#a7acfa",
+          400: "#8784f5",
+          500: "#7266ee",
+          600: "#563adf",
+          700: "#563bc7",
+          800: "#4533a0",
+          900: "#3c307f",
+          950: "#241c4a",
+        },
+      },
+    },
   },
   plugins: [],
 };
